b2b62088b5121ca58bf0169821d2faabf6435c3a
alqamah <alqama.hasnain@gmail.com>
2024-07-18 20:39:05 +0530

    added screens, styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 import Dropdown from './components/Dropdown';
 
